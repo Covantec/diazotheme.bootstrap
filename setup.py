dad43ad56c63b0c086568e32d586f7de081441a8
Leonardo J. Caballero G <leonardocaballero@gmail.com>
2018-07-04 21:07:50 -0400

    Updated the description metadata setuptools for this package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.0'
 
 setup(name='diazotheme.bootstrap',
       version=version,
-      description="A Diazo Themes implementation for Twitter Bootstrap CSS",
+      description="The Diazo Themes implementation for Twitter Bootstrap CSS",
       long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
